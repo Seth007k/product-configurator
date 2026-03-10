209e7bca89d41ba1acb1a8ab95725ce064c6c55e
Seth007k <107913274+Seth007k@users.noreply.github.com>
2026-03-10 12:32:02 +0100

    added variant.schema

---
+++ Backend/src/modules/products/schemas/variant.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { Product } from './product.schema';
+
+export type VariantDocument = Variant & Document;
+
+@Schema({ timestamps: true })
+export class Variant {
+    @Prop({ type: String, required: true })
+    value: string;
+
+    @Prop({ type: Types.ObjectId, ref: Product.name, required: true })
+    product: Product;
+
+    @Prop({ type: [{ baureihe: String, modelle: [String] }], required: true })
+    assignments: { baureihe: string; modelle: string[] }[];
+}
+
+export const VariantsSchema = SchemaFactory.createForClass(Variant);
