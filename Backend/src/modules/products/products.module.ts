2e985c50926940d36d911a41994b2b4eb09efacb
Seth007k <107913274+Seth007k@users.noreply.github.com>
2026-03-10 12:30:40 +0100

    added VariantsSchema, Serviuce and controller

--- Backend/src/modules/products/products.module.ts
+++ Backend/src/modules/products/products.module.ts
@@ -3,10 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product, ProductSchema } from './schemas/product.schema';
+import { Variant, VariantsSchema } from './schemas/variant.schema';
+import { VariantsController } from './variant.controller';
+import { VariantsService } from './variants.service';
+
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
-  controllers: [ProductsController],
-  providers: [ProductsService]
+  imports: [MongooseModule.forFeature([
+    { name: Product.name, schema: ProductSchema },
+    { name: Variant.name, schema: VariantsSchema },
+  ]),
+  ],
+  controllers: [ProductsController, VariantsController],
+  providers: [ProductsService, VariantsService],
 })
 export class ProductsModule { }
