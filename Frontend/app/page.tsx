7829256bbfe3825bfb344d8e419636c1f3f100b7
Seth007k <107913274+Seth007k@users.noreply.github.com>
2026-03-12 13:12:13 +0100

    implement home page with productlist

---
+++ Frontend/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ProductList from "../components/ProductList";
+
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Produkt Konfigurator</h1>
+
+      <ProductList />
+    </main>
+  );
+}
